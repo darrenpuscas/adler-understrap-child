aa6cd60fba2b37024337894c95e9601f78e0285a
Michael Smith <mlsmith45@gmail.com>
2022-10-11 16:55:42 -0400

    adding ACF fields prog

--- page-templates/Indiv Programs.php
+++ page-templates/Indiv Programs.php
@@ -30,26 +30,21 @@ if ( is_front_page() ) {
 								<div class="indiv-programs-masthead-content standard-masthead-content pt-6 pb-6 col-md-10 offset-md-1">
 									<section class="indiv-programs-masthead-text masthead-section">
 										<div class="indiv-programs-main-header standard-main-header display-1">
-											<!-- Pull in title -->
 											<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>	
-											<!-- <h1>Certificate in CBT for Clinical Practitioners</h1> -->
 										</div><!-- end class="indiv-programs-main-header standard-main-header display-1" -->
-										
-										<div class="indiv-programs-sub-statement standard-sub-statement"><h5>Program Overview</h5></div>
-											<!-- ACF - program_sub_heading -->
-
+										<div class="indiv-programs-sub-statement standard-sub-statement"><h5><?php the_field('prog_sub_heading'); ?></h5></div>
 										<div class="indiv-programs-text standard-text">
-											<!-- ACF program_desc_text -->
-											<p>The online CBT program is a platform for learning the skills of cognitive behavioural therapy. Approved by the Academy of Cognitive Therapy and taught by Beck-trained clinicians, this program is comprehensive in nature. It is designed with a broad audience of students and practice types in mind. Please note that oral and written fluency in English is required. During their course of studies, students proceed through written lectures, talks, case studies, video demonstrations and professional practice exercises. Students will submit work for each of the 12 modules and will receive personalized, attentive feedback all throughout the course. Course content is varied and made up of lectures, viewings, demonstrations, supplemental readings, and professional/reflective exercises. There are no tests or exams, however, there are assignments for each module, which must be submitted online. Upon completion and submission of all assignments, and following their review by the teaching team, students will receive a Certificate of Completion at the end of each course.</p>
-											<p>The program is divided into two levels. In Level 1, CBT - Foundations, students learn the fundamentals of the CBT modality. In Level 2, CBT - Advanced Practice, students learn how to apply these interventions by choosing one of three streams (students can take more than one stream if they wish). Students will leave the course with a broad base of understanding in this important modality and can go on to apply their training across a wide variety of contexts.</p>
-											<p>All courses within the program require 40 hours of study. Students may choose to end their training after Level 1, or progress onward to one of three streams in Level 2. Upon completion of each course, students will have obtained strong academic foundations and skills-based knowledge, as well as a toolkit of worksheets available for duplication to use in their practice. Please note that to be eligible for Level 2, students must have completed Level 1 (or an approved equivalent, see below). The cost for each course is $895 + HST and these must be completed within a maximum of 12 weeks. </p>
+											<!-- ACF prog_desc_text -->
+											<?php the_field('prog_overview_text'); ?>
 										</div> <!-- end class="indiv-programs-text standard-text" -->
 									
 									</section><!-- end class="indiv-programs-masthead-text masthead-section" -->
 									
 									<section class="indiv-programs-masthead-image masthead-section">
-										<!-- ACF program_desc_img -->
-										<img src="http://adlerup.local/wp-content/uploads/grad-image.png" class="cerfification-masthead" alt="">
+										<?php $image = get_field('prog_overview_image');
+											if( !empty( $image ) ): ?>
+    										<img src="<?php echo esc_url($image['url']); ?>" class="cerfification-masthead" alt="<?php echo esc_attr($image['alt']); ?>" />
+											<?php endif; ?>
 									</section>
 								</div><!-- end masthead content -->
 							</div><!-- end row -->
@@ -57,25 +52,30 @@ if ( is_front_page() ) {
 					</article>
 
 					<article id="indiv-programs-1">
-
 						<div class="indiv-programs-course-container standard-course-container container-xl pt-6">
 							<div class="row">
-								<section class="indiv-programs1-intro standard-intro masthead-section">
-									<div class="indiv-programs-intro-headline standard-intro-headline display-3">
-										<!-- ACF = program_  -->
-									
-									<h3>CBT Courses</h3></div>
-								</section>
-								<div class="indiv-programs-course-content standard-course-content pb-4 col-md-10 col-lg-10 offset-md-1 col-sm-12"> 
+								<?php $title = get_field('prog_section_title_1');
+								if( $title ): ?>
+										<section class="indiv-programs1-intro standard-intro masthead-section">
+											<div class="indiv-programs-intro-headline standard-intro-headline display-3">					
+												<h3><?php the_field('prog_section_title_1') ?></h3>
+											</div>
+										</section>
+								<?php endif ?>
+
+
+								<div class="indiv-programs-course-content standard-course-content pb-4 col-md-10 col-lg-10 offset-md-1 col-sm-12">
+								<!-- Box 1 -->
+								<?php $box1 = get_field('prog_box_1'); ?>
 									<section class="indiv-programs-course-image certification-course-section">
-										<img src="http://adlerup.local/wp-content/uploads/grad-image.png" class="cerfification-masthead" alt="">
+										<img src="<?php echo esc_url( $box1['image']['url'] ); ?>" class="cerfification-masthead" alt="">
 									</section>
-									<section class="indiv-programs-course standard-course">
-										<h5 class="text-uppercase">LEVEL 1: The Fundamentals of Cognitive Behavioural Therapy</h5>
-											<p>This course is a thorough introduction to the foundations of practice in cognitive behavioural therapy across the lifespan, including with adults, children and youth. Designed with the cultural and contextual implications of a wide variety of students in mind, students will begin practice right away, putting to use a course-pack of 20 worksheets (the CBT “interventions”) that can be duplicated for practice.</p>
-											<p>REQUIRED TEXTBOOK: Josefowitz, Nina and Myran, David (2021). CBT Made Simple: A Clinician’s Guide to Practicing Cognitive Behavioural Therapy (2nd ed.). Oakland, CA: New Harbinger Publications</p>	
-										<button class="btn btn-primary">Register</button>
-									</section>
+										<section class="indiv-programs-course standard-course">
+											<h5 class="text-uppercase"><?php echo $box1['title'] ?></h5>
+											<p><?php echo $box1['text'] ?></p>
+											<?php echo 'URL Field'; echo $box1['button_url'] ?>
+											<a href="<?php esc_url( $box1['button_url'] ) ?>" class="btn btn-primary"><?php echo box1['button'] ?></a>
+										</section>
 								</div><!-- end column -->
 							</div><!-- end row -->
 						</div><!-- end container  -->
