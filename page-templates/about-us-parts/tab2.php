658abbed35724ede76ab0efb4a730b4ef83e336b
Michael Smith <mlsmith45@gmail.com>
2023-01-04 17:12:05 -0500

    Working on about us

--- page-templates/about-us-parts/tab2.php
+++ page-templates/about-us-parts/tab2.php
@@ -1,18 +1,32 @@
-<h3 class="pb-2 pb-sm-1 pb-md-1">Who we are</h3>
+<h3 class="pb-2 pb-sm-1 pb-md-1"><?php the_field('heading_2'); ?></h3>
+
+<?php
+$group2 = get_field('group_2');
+if( $group2 ): ?>
+  
 <ul class="nav nav-tabs pt-2 pt-md-2" id="myTab" role="tablist">
 	<li class="nav-item" role="presentation">    
-		<div class="nav-link active" id="faculty-adler-tab" data-bs-toggle="tab" data-bs-target="#faculty-adler" type="button" role="tab" aria-controls="faculty-adler" aria-selected="true"><h5>Mission and Values</h5></button>
+		<div class="nav-link active" id="faculty-adler-tab" data-bs-toggle="tab" data-bs-target="#faculty-adler" type="button" role="tab" aria-controls="faculty-adler" aria-selected="true"><h5><?php echo esc_attr( $group2['label_1']); ?></h5></button>
 	</li>
 	<li class="nav-item" role="presentation">
-		<div class="nav-link" id="administration-adler-tab" data-bs-toggle="tab" data-bs-target="#administration-adler" type="button" role="tab" aria-controls="administration-adler" aria-selected="false"><h5>Our Administration</h5></button>
+		<div class="nav-link" id="administration-adler-tab" data-bs-toggle="tab" data-bs-target="#administration-adler" type="button" role="tab" aria-controls="administration-adler" aria-selected="false"><h5><?php echo esc_attr( $group2['label_2']); ?></h5></button>
 	</li>
 	<li class="nav-item" role="presentation">
-		<button class="nav-link" id="history-adler-tab" data-bs-toggle="tab" data-bs-target="#history-adler" type="button" role="tab" aria-controls="history-adler" aria-selected="false"><h5>Our history</h5></button>
+		<button class="nav-link" id="history-adler-tab" data-bs-toggle="tab" data-bs-target="#history-adler" type="button" role="tab" aria-controls="history-adler" aria-selected="false"><h5><?php echo esc_attr( $group2['label_3']); ?></h5></button>
 	</li>
 </ul>
 
+<?php endif; ?>
 
 <div class="tab-content" id="tab-2-content">
+	<div class="tab-pane fade" id="history-adler" role="tabpanel" aria-labelledby="history-adler-tab">
+			mission and values text
+	</div>
+
+
+
+
+
 	<div class="tab-pane fade show active" id="faculty-adler" role="tabpanel" aria-labelledby="faculty-adler-tab">
 	<?php
 		// repeater in a group
