2b3a302521fb12e9778fc1850766cab8baa67938
Michael Smith <mlsmith45@gmail.com>
2023-01-09 13:10:37 -0500

    All Tabs Working

--- page-templates/about-us-parts/tab2.php
+++ page-templates/about-us-parts/tab2.php
@@ -6,41 +6,18 @@ if( $group2 ): ?>
   
 <ul class="nav nav-tabs pt-2 pt-md-2" id="myTab" role="tablist">
 	<li class="nav-item" role="presentation">    
-		<div class="nav-link active" id="faculty-adler-tab" data-bs-toggle="tab" data-bs-target="#faculty-adler" type="button" role="tab" aria-controls="faculty-adler" aria-selected="true"><h5><?php echo esc_attr( $group2['label_1']); ?></h5></button>
+		<button class="nav-link active" id="mission-adler-tab" data-bs-toggle="tab" data-bs-target="#mission-adler" type="button" role="tab" aria-controls="mission-adler" aria-selected="true"><h5><?php echo esc_attr( $group2['label_1']); ?></h5></button>
 	</li>
 	<li class="nav-item" role="presentation">
-		<div class="nav-link" id="administration-adler-tab" data-bs-toggle="tab" data-bs-target="#administration-adler" type="button" role="tab" aria-controls="administration-adler" aria-selected="false"><h5><?php echo esc_attr( $group2['label_2']); ?></h5></button>
+		<button class="nav-link" id="administration-adler-tab" data-bs-toggle="tab" data-bs-target="#administration-adler" type="button" role="tab" aria-controls="administration-adler" aria-selected="false"><h5><?php echo esc_attr( $group2['label_2']); ?></h5></button>
 	</li>
 	<li class="nav-item" role="presentation">
 		<button class="nav-link" id="history-adler-tab" data-bs-toggle="tab" data-bs-target="#history-adler" type="button" role="tab" aria-controls="history-adler" aria-selected="false"><h5><?php echo esc_attr( $group2['label_3']); ?></h5></button>
 	</li>
 </ul>
-
-<?php endif; ?>
-
 <div class="tab-content" id="tab-2-content">
-	<div class="tab-pane fade" id="history-adler" role="tabpanel" aria-labelledby="history-adler-tab">
+	<div class="tab-pane fade show active" id="mission-adler" role="tabpanel" aria-labelledby="mission-adler-tab">
 			mission and values text
-	</div>
-
-
-
-
-
-	<div class="tab-pane fade show active" id="faculty-adler" role="tabpanel" aria-labelledby="faculty-adler-tab">
-	<?php
-		// repeater in a group
-		if( have_rows('your_group') ): while ( have_rows('your_group') ) : the_row(); 
-
-				if( have_rows('your_repeater') ): while ( have_rows('your_repeater') ) : the_row();       
-
-						echo get_sub_field('repeater_sub_field');
-
-				endwhile; endif;
-
-		endwhile; endif;
-
-		?>
 	</div>
 	<div class="tab-pane fade" id="administration-adler" role="tabpanel" aria-labelledby="administration-adler-tab">
 		DB Query to Admin CPT
@@ -76,3 +53,5 @@ if( $group2 ): ?>
 		adler history text
 	</div>		
 </div>
+
+<?php endif; ?>
