170ca872065a7ae973b92c88d0816c815f1fc0f5
Michael Smith <mlsmith45@gmail.com>
2023-02-27 12:15:01 -0500

    need img on left

--- page-templates/Indiv Program.php
+++ page-templates/Indiv Program.php
@@ -20,83 +20,82 @@ if ( is_front_page() ) {
 
 <div id="full-width-page-wrapper" class="wrapper">
 	<div class="col-md-12 content-area" id="primary">
+		<article class="site-main" id="main" role="main">
+			<div id="indiv-programs-masthead">
+				<main id="coaching-grid" class="indiv-programs-course-content standard-course-content pb-4 col-md-10 col-lg-10 offset-md-1 col-sm-12">
 
-		<main class="site-main" id="main" role="main">
 
-			<section id="indiv-programs-masthead">
-				<div class="indiv-programs-masthead-container container-xl">
-					<div class="indiv-programs-masthead-content standard-masthead-content pt-6 pb-6 col-md-10 offset-md-1">
-						<div class="indiv-programs-masthead-text masthead-section">
-							<div class="indiv-programs-main-header standard-main-header display-1"><?php the_title( '<h1 class="text-uppercase">', '</h1>' ); ?></div>
 
-							<?php if ( have_rows( 'introduction' ) ) : ?>
-								<?php while ( have_rows( 'introduction' ) ) : the_row(); ?>
-								<div class="indiv-programs-sub-statement standard-sub-statement"><?php the_sub_field( 'sub_title' ); ?></div>
-									<div class="indiv-programs-text standard-text"><?php the_sub_field( 'introduction_text' ); ?></div>
+				<section class="indiv-programs-masthead-text masthead-section">
+					<div class="indiv-programs-main-header standard-main-header display-1"><?php the_title( '<h1 class="text-uppercase">', '</h1>' ); ?></div>
+
+						<?php if ( have_rows( 'introduction' ) ) : ?>
+							<?php while ( have_rows( 'introduction' ) ) : the_row(); ?>
+						<div class="indiv-programs-sub-statement standard-sub-statement"><?php the_sub_field( 'sub_title' ); ?></div>
+						<div class="indiv-programs-text standard-text"><?php the_sub_field( 'introduction_text' ); ?></div>
 									
-							</div><!-- Section  -->
-							<div class="indiv-programs-masthead-image masthead-section">
-								<?php $introduction_image = get_sub_field( 'introduction_image' ); ?>
-									<?php if ( $introduction_image ) : ?>
-										<img src="<?php echo esc_url( $introduction_image['url'] ); ?>" alt="<?php echo esc_attr( $introduction_image['alt'] ); ?>" />
-									<?php endif; ?>
-									</div>
-								<?php endwhile; ?>
-							<?php endif; ?>
-					</div>
+				</section><!-- Section  -->
+
+
+				<section class="indiv-programs-masthead-image masthead-section">
+					<?php $introduction_image = get_sub_field( 'introduction_image' ); ?>
+						<?php if ( $introduction_image ) : ?>
+							<img src="<?php echo esc_url( $introduction_image['url'] ); ?>" alt="<?php echo esc_attr( $introduction_image['alt'] ); ?>" />
+						<?php endif; ?>
+						</div>
+					<?php endwhile; ?>
+				<?php endif; ?>
 				</div>
+			</div>
 			</section>
 
-			<section id="indiv-programs-1">
-				<div class="indiv-programs-course-container standard-course-container container-xl pt-6">
-					<div class="indiv-programs1-intro standard-intro masthead-section">
-						<?php if ( have_rows( 'section 2' ) ) : ?>
-						<?php while ( have_rows( 'section 2' ) ) : the_row(); ?>
+							<?php if ( have_rows( 'section 2' ) ) : ?>
+						<?php while ( have_rows( 'section 2' ) ) : the_row(); ?>	
+
+				<section class="indiv-programs-course-container standard-course-container container-xl pt-6">
+				<?php $section_image = get_sub_field( 'section_image' ); ?>
+								<?php if ( $section_image ) : ?>
+
+						
+						<div class="indiv-programs-course-image certification-course-section">
+							<img src="<?php echo esc_url( $section_image['url'] ); ?>" alt="<?php echo esc_attr( $section_image['alt'] ); ?>" />
+						</div>
+								<?php endif; ?>
+
+								<div class="indiv-programs1-intro standard-intro masthead-section">
+
+
 						<h3 class="standard-intro-headline"><?php the_sub_field( 'section_title' ); ?></h3>
 						<?php the_sub_field( 'section_text' ); ?>
-						<div class="indiv-programs-intro-headline standard-intro-headline display-3">
-						</div>
 					</div>
+				</div>
 
-					<div class="indiv-programs-course-content standard-course-content pb-4 col-md-10 col-lg-10 offset-md-1 col-sm-12">
-							<?php $section_image = get_sub_field( 'section_image' ); ?>
-								<?php if ( $section_image ) : ?>
-							<div class="indiv-programs-course-image certification-course-section">
-								<img src="<?php echo esc_url( $section_image['url'] ); ?>" alt="<?php echo esc_attr( $section_image['alt'] ); ?>" />
-							</div>
-									<?php endif; ?>
 								<?php endwhile; ?>
-						</div>
-							<?php endif; ?>	
 					</div>
+						<?php endif; ?>	
 				</section>
+	
 
-
-				<section class="section-tertiary-background">
+			<section class="section-tertiary-background">
 				<div class="indiv-programs-course-container standard-course-container container-xl pt-6">
 					<div class="indiv-programs1-intro standard-intro masthead-section">
 					<?php if ( have_rows( 'course_1' ) ) : ?>
 						<?php while ( have_rows( 'course_1' ) ) : the_row(); ?>
-								<div class="indiv-programs-course standard-course">
-									<h3><?php the_field( 'courses_title' ); ?></h3>
-
-											<h5 class="text-uppercase"><?php the_sub_field( 'course_title' ); ?></h5>
-
-										<p><?php the_sub_field( 'course_description' ); ?>	
-									<a href="<?php the_sub_field( 'button_url' ); ?>" class="btn btn-primary"><?php the_sub_field( 'button_label' ); ?></a href="">
-								</div>
-										<?php endwhile; ?>
-									<?php endif; ?>
-
+						<div class="indiv-programs-course standard-course">
+							<h3><?php the_field( 'courses_title' ); ?></h3>
+									<h5 class="text-uppercase"><?php the_sub_field( 'course_title' ); ?></h5>
+								<p><?php the_sub_field( 'course_description' ); ?>	
+							<a href="<?php the_sub_field( 'button_url' ); ?>" class="btn btn-primary"><?php the_sub_field( 'button_label' ); ?></a href="">
 						</div>
+							<?php endwhile; ?>
+						<?php endif; ?>
+					</div>
 				</div>		
 			</section>
 
 			<section id="indiv-programs-additional">
 				<div class="indiv-programs-additional standard-course-container container-xl">
 					<div class="indiv-programs-additional-course-content standard-course-content pt-2 pt-md-5 pb-2 pb-md-5 col-md-10 col-lg-10 offset-md-1 col-sm-12">
-
-
 					<?php if ( have_rows( 'course_2' ) ) : ?>
 						<div class="indiv-programs-additional-course standard-course">
 
@@ -113,10 +112,16 @@ if ( is_front_page() ) {
 					<?php endif; ?>
 
 					</div>
-				</div>		
-			</section>
+				</section>
 
-		</main><!-- #main -->
+
+
+
+
+				</main><!-- Coaching Grid -->
+			</div><!-- Masthead -->
+
+		</article><!-- #main -->
 
 	</div><!-- #primary -->
 
