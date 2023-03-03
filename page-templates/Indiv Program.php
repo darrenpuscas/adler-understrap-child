f1207c950d6b658b88244e66aa06b0fca048200f
Michael Smith <mlsmith45@gmail.com>
2023-03-03 16:42:48 -0500

    before change course2 --> Repeater

--- page-templates/Indiv Program.php
+++ page-templates/Indiv Program.php
@@ -20,9 +20,9 @@ if ( is_front_page() ) {
 
 <div id="full-width-page-wrapper" class="wrapper">
 	<div class="col-md-12 content-area" id="primary">
-		<article class="site-main" >
-			<div id="indiv-programs-masthead">
-				<main id="main" role="main" id="coaching-grid" class="iindiv-programs-masthead-content standard-masthead-content pt-6 pb-6 col-md-10 offset-md-1">
+		<main class="site-main" id="main" role="main" >
+			<article id="indiv-programs-masthead">
+				<div id="coaching-grid-img_R1" class="indiv-programs-masthead-content standard-masthead-content pt-6 pb-6 col-md-10 offset-md-1">
 
 
 				<section class="indiv-programs-masthead-text masthead-section">
@@ -34,37 +34,68 @@ if ( is_front_page() ) {
 						<div class="indiv-programs-text standard-text"><?php the_sub_field( 'introduction_text' ); ?></div>
 									
 				</section><!-- Section: Intro  -->
-				<aside class="indiv-programs-masthead-image masthead-section ">
+				<figure class="indiv-programs-masthead-image masthead-section ">
 					<?php $introduction_image = get_sub_field( 'introduction_image' ); ?>
 						<?php if ( $introduction_image ) : ?>
 							<img src="<?php echo esc_url( $introduction_image['url'] ); ?>" alt="<?php echo esc_attr( $introduction_image['alt'] ); ?>" />
 					<?php endif; ?>
-				</aside>
+				</figure>
 
 						<?php endwhile; ?>
 						<?php endif; ?>
 
-							<?php if ( have_rows( 'section 2' ) ) : ?>
-						<?php while ( have_rows( 'section 2' ) ) : the_row(); ?>	
+			</div>
+			<!-- coaching grid -ImgR1 -->
+<!-- ------------------------------------------------------------------------------------ -->
 
-				<section class="indiv-programs-course-container standard-course-container section-white-background container-xl pt-6">
+
+							<?php if ( have_rows( 'section 2' ) ) : ?>
+						<?php while ( have_rows( 'section 2' ) ) : the_row(); ?>
+			<div id="coaching-grid-img_L1" class="indiv-programs-img_l-grid offset-md-1 col-md-10">
+				<!-- Coach Eduction Image -->
+				<figure class="indiv-programs-course-container standard-course-container section-white-background container-xl pt-6">
+					<p>section image</p>
+
+<!-- What I really want is to span this over the entire main grid row and float the image left -->
 				<?php $section_image = get_sub_field( 'section_image' ); ?>
 					<?php if ( $section_image ) : ?>					
 						<div class="indiv-programs-course-image certification-course-section">
 							<img src="<?php echo esc_url( $section_image['url'] ); ?>" alt="<?php echo esc_attr( $section_image['alt'] ); ?>" />
 						</div>
 					<?php endif; ?>
+				</figure>
+
+
+
+
+
+
+				<section class="indiv-programs-course-container standard-course-container section-white-background container-xl pt-6">
 					<div class="indiv-programs1-intro standard-intro masthead-section">
 						<h3 class="standard-intro-headline"><?php the_sub_field( 'section_title' ); ?></h3>
+						<p>Section Title</p>
 						<?php the_sub_field( 'section_text' ); ?>
 					</div>
+				</section>
+
+
+
+
+
 							<?php endwhile; ?>
 						<?php endif; ?>	
-				</section><!-- Coach Eduction  -->
-	
 
+			</div>
+		<!-- Grid #img_L1 -->
+
+
+
+
+	
 			<section class="section-tertiary-background">
+				<!-- Single Coaching Course -->
 				<div class="indiv-programs-course-container standard-course-container container-xl pt-6">
+					<p>Single Coaching Course</p>
 					<div class="indiv-programs1-intro standard-intro masthead-section">
 					<?php if ( have_rows( 'course_1' ) ) : ?>
 						<?php while ( have_rows( 'course_1' ) ) : the_row(); ?>
@@ -78,40 +109,45 @@ if ( is_front_page() ) {
 						<?php endif; ?>
 					</div>
 				</div>		
-			</section><!-- Single Coaching Course -->
+			</section>
 
-			<section id="indiv-programs-additional">
-				<div class="indiv-programs-additional standard-course-container container-xl">
-					<div class="indiv-programs-additional-course-content standard-course-content pt-2 pt-md-5 pb-2 pb-md-5 col-md-10 col-lg-10 offset-md-1 col-sm-12">
+				<section id="indiv-programs-additional">
+					<p>Course 2</p>
+					<div class="indiv-programs-additional standard-course-container container-xl">
+						<div class="indiv-programs-additional-course-content standard-course-content pt-2 pt-md-5 pb-2 pb-md-5 col-md-10 col-lg-10 offset-md-1 col-sm-12">
+
+						<?php if ( have_rows( 'course_2' ) ) : ?>
+							
+							<aside class="indiv-programs-additional-course standard-course">
+
+							<?php while ( have_rows( 'course_2' ) ) : the_row(); ?>
+
+								<div class="indiv-course-titlearea">
+									<img src="http://adlerup.local/wp-content/uploads/noun-leaf.png">
+									<h6 class="indiv-course-title text-uppercase"><?php the_sub_field( 'course_title' ); ?></h6>
+								</div>	
+								<div class="indiv-course-timing"><?php the_sub_field( 'course_description' ); ?></div>
+								<a href="<?php the_sub_field( 'button_url' ); ?>" class="btn btn-primary"><?php the_sub_field( 'button_label' ); ?></a>
+								<?php endwhile; ?>
+							</aside>
+						<?php endif; ?>
 
 
 
-					<?php if ( have_rows( 'course_2' ) ) : ?>
+
+
 						
-						<aside class="indiv-programs-additional-course standard-course">
 
-						<?php while ( have_rows( 'course_2' ) ) : the_row(); ?>
-
-							<div class="indiv-course-titlearea">
-								<img src="http://adlerup.local/wp-content/uploads/noun-leaf.png">
-								<h6 class="indiv-course-title text-uppercase"><?php the_sub_field( 'course_title' ); ?></h6>
-							</div>	
-							<div class="indiv-course-timing"><?php the_sub_field( 'course_description' ); ?></div>
-							<a href="<?php the_sub_field( 'button_url' ); ?>" class="btn btn-primary"><?php the_sub_field( 'button_label' ); ?></a>
-							<?php endwhile; ?>
-						</aside>
-					<?php endif; ?>
-
+						</div>
 					</div>
-				</div>
 
 				</section><!-- Invididual Course List: Modals -->
 
-				</main><!-- Coaching Grid -->
+				</div><!-- Coaching Grid -->
 
-			</div><!-- Masthead -->
+			</article><!-- Masthead -->
 
-		</article><!-- #main -->
+		</main><!-- #main -->
 
 	</div><!-- #primary -->
 
