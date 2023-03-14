2f28bff8cda521a3843f712332bbe7e39dfe0733
Michael Smith <mlsmith45@gmail.com>
2023-03-14 14:49:12 -0400

    Tabs working - need formatting

--- page-templates/about-flex.php
+++ page-templates/about-flex.php
@@ -19,42 +19,22 @@ if ( is_front_page() ) {
 ?>
 
 <div id="full-width-page-wrapper" class="wrapper">
-	<div class="content-area" id="primary">
-		<main class="site-main" id="main" role="main">
-			<div id="standard-masthead" class="standard-with-menu">	
-				
-			
-			<section class="indiv-programs-masthead-text masthead-section">
-				<div class="indiv-programs-main-header standard-main-header display-1">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>	
-				</div><!-- end class="indiv-programs-main-header standard-main-header display-1" -->
-				<div class="indiv-programs-sub-statement standard-sub-statement"><?php the_field( 'page_introduction' ); ?></div>
-				
-			
-			</section><!-- end class="indiv-programs-masthead-text masthead-section" -->
-
-				<!-- <article class="container-xl">
-					<div class="menu-section masthead-section why-adler-masthead-content pt-4 pt-md-6 pb-4 pb-md-3 col-md-10 offset-md-1">
-							<header class="entry-header why-adler-title">
-								<?php //the_title( '<h1 class="text-uppercase">', '</h1>' ); ?>
-							</header> -->
-							
-							
-							
+	<div class="col-md-12 content-area" id="primary">
+		<main class="site-main" id="main" role="main" >
+			<div class="section-tertiary-background">
+				<div class="container-xl">
+					<header class="indiv-programs-masthead-text masthead-section ">
+						<div class="display-1"><?php the_title( '<h1 class="text-uppercase">', '</h1>' ); ?></div>	
+					</header>
+					<section id="about-us-masthead" class="pt-6 pb-6 col-md-10 offset-md-1 ">
+						<?php the_field( 'page_introduction' ); ?>
+					</section>
 							<!-- Group 1 Parts -->
-
-							
-
-
-
-							
-								<?php get_template_part('page-templates/about-us-flex-parts/flex-1'); ?>
-								
-								<!-- Group 2 Part -->
-
-					<!-- </div> -->
-					<!-- Why Adler masthead class -->				
-				<!-- </article> -->
+					<section>
+						<?php get_template_part('page-templates/about-us-flex-parts/flex-1'); ?>
+				</section>
+								<!-- Group 2 Part -->		
+				</div><!-- container xl -->
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- page-templates/about-us-flex-parts/flex-1.php
+++ page-templates/about-us-flex-parts/flex-1.php
@@ -4,31 +4,29 @@
 
 	<?php while ( have_rows( 'tab_list' ) ) : the_row(); ?>
 		<?php if ( get_sub_field( 'active' ) == 1 ) : ?>
-			<?php echo 'true'; ?>
-			<li class="nav-item" role="presentation"> 
+			<li class="nav-item" role="presentation">
 				<button
 					class="nav-link active" 
-					id="<?php the_sub_field('tab_label');?>-tab"
+					id="<?php the_sub_field('tab_name');?>-tab"
 					data-bs-toggle="tab" 
-					data-bs-target="#<?php the_sub_field('tab_label');?>" 
+					data-bs-target="#<?php the_sub_field('tab_name');?>" 
 					type="button" role="tab" 
-					aria-controls="<?php the_sub_field('tab_label');?>" 
+					aria-controls="<?php the_sub_field('tab_name');?>" 
 					aria-selected="true">
-					<h5><?php the_sub_field( 'tab_name' ); ?></h5>
+					<h5><?php the_sub_field( 'tab_label' ); ?></h5>
 				</button>
 			</li>
 		<?php else : ?>
-			<?php echo 'false'; ?>
 			<li class="nav-item" role="presentation"> 
 				<button
 					class="nav-link" 
-					id="<?php the_sub_field('tab_label');?>-tab"
+					id="<?php the_sub_field('tab_name');?>-tab"
 					data-bs-toggle="tab" 
-					data-bs-target="#<?php the_sub_field('tab_label');?>" 
+					data-bs-target="#<?php the_sub_field('tab_name');?>" 
 					type="button" role="tab" 
-					aria-controls="<?php the_sub_field('tab_label');?>" 
+					aria-controls="<?php the_sub_field('tab_name');?>" 
 					aria-selected="true">
-					<h5><?php the_sub_field( 'tab_name' ); ?></h5>
+					<h5><?php the_sub_field( 'tab_label' ); ?></h5>
 				</button>
 			</li>
 		<?php endif; ?>
@@ -38,25 +36,58 @@
 	<?php echo('<p>No Tabs Found</p>') ?>
 <?php endif; ?>
 
+</div>
+<!-- add console log for php -->
+<?php
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
 
+?>
 
 
 <?php if ( have_rows( 'tab_group_1_content' ) ): ?>
+<section class="tab-content" id="tab-1-content">
 	<?php while ( have_rows( 'tab_group_1_content' ) ) : the_row(); ?>
-		<?php if ( get_row_layout() == 'tab_group' ) : ?>
+
+	<?php if ( get_row_layout() == 'tab_content' ) : ?>
+		<?php $tabName = get_sub_field( 'tab_name' ) ?>
+	<?php endif ?>
+
+		<div 
+			class="tab-pane fade show active mb-5" id="<?php echo $tabName?>" role="tabpanel" aria-labelledby="<?php echo $tabName?>">
+
+
+	<?= console_log($tabName); ?>
+
+
+			<?php if ( get_row_layout() == 'tab_content' ) : ?>
+			<?php //the_sub_field( 'tab_name' ); ?>
+
+
 			<?php if ( have_rows( 'paragraph' ) ) : ?>
 				<?php while ( have_rows( 'paragraph' ) ) : the_row(); ?>
 					<?php the_sub_field( 'text' ); ?>
-					<?php the_sub_field( 'image_position' ); ?>
-					<?php if ( get_sub_field( 'drop_cap' ) == 1 ) : ?>
+					<?php //the_sub_field( 'image_position' ); ?>
+					<?php //if ( get_sub_field( 'drop_cap' ) == 1 ) : ?>
 						<?php // echo 'true'; ?>
-					<?php else : ?>
+					<?php //else : ?>
 						<?php // echo 'false'; ?>
-					<?php endif; ?>
+					<?php //endif; ?>
+
 				<?php endwhile; ?>
 			<?php else : ?>
 				<?php // No rows found ?>
 			<?php endif; ?>
+
+
+
+
 			<?php if ( have_rows( 'bullet_list' ) ) : ?>
 				<?php while ( have_rows( 'bullet_list' ) ) : the_row(); ?>
 					<?php the_sub_field( 'content' ); ?>
@@ -72,7 +103,14 @@
 				<?php // No rows found ?>
 			<?php endif; ?>
 		<?php endif; ?>
+
+		</div><!-- tab1 group content -->
 	<?php endwhile; ?>
+
+
 <?php else: ?>
-	<?php // No layouts found ?>
+	<?php echo 'No layouts found' ?>
+
+
+</section>
 <?php endif; ?>
