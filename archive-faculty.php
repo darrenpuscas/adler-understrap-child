372176e2acb447004d36fc2548b521e8b999bc53
Michael <mlsmith45@gmail.com>
2024-09-26 19:32:19 -0400

    before custom query

--- archive-faculty.php
+++ archive-faculty.php
@@ -45,7 +45,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
+						get_template_part( 'archive-structure/faculty', get_post_format() );
+
 					}
 				} else {
 					get_template_part( 'loop-templates/content', 'none' );
